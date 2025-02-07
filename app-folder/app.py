7963a3e2acfec22b653380ea5871f8a98cb9aed2
Pavel Traian-Lucian <traian.pavel.22@student.politice.ro>
2025-02-07 18:49:06 +0000

    corrected the title

--- app-folder/app.py
+++ app-folder/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def titlu():
-    return "Aplicație pentru datoriigit-commi" 
+    return "Aplicație pentru datorii" 
 
 if __name__ == "main":
     app.run()
