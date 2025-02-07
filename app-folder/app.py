07adf0396a306d4cfc37f2f5c1c3d57ff54fc485
Pavel Traian-Lucian <traian.pavel.22@student.politice.ro>
2025-02-07 18:48:41 +0000

    [skip ci]

--- app-folder/app.py
+++ app-folder/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def titlu():
-    return "Aplicație pentru datorii" 
+    return "Aplicație pentru datoriigit-commi" 
 
 if __name__ == "main":
     app.run()
